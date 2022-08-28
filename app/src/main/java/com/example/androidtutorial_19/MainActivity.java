af19d6c783ec0cbaa65f63faf5045dfa7eef26a4
root <it21034268@my.sliit.lk>
2022-08-28 12:30:49 +0530

    AndroidTutorial_19

---
+++ app/src/main/java/com/example/androidtutorial_19/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.androidtutorial_19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String week[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+
+        //In here provide application context "this"
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this,R.layout.single_row,R.id.textView,week);
+
+        listView = findViewById(R.id.list_view);
+        listView.setAdapter(arrayAdapter);
+
+    }
+}
